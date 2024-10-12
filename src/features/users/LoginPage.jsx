307f3226ba161e0230f875b0c0687531c798c1eb
Nazgul <bizdanistar@gmail.com>
2024-10-12 13:13:36 -0500

    main

--- src/features/users/LoginPage.jsx
+++ src/features/users/LoginPage.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiLock } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
+import { useDispatch, useSelector } from 'react-redux';
+import { authentification } from './loginSlice';
+
+
 const LoginPage = () => {
+  const dispatch = useDispatch()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const onUserNameChanged = e => setUsername(e.target.value)
+  const onPasswordChanged = e => setPassword(e.target.value)
+  const errorMessage = useSelector((state) => state.isLogged.errorMessage);
+
+  
+
+  const onSubmit = () => {
+    if (username && password) {
+      // Dispatch the authentication action with the entered username and password
+      dispatch(authentification({ username, password }));
+      
+      // Reset the input fields
+      setUsername('');
+      setPassword('');
+    }
+  };
+
   return (
     <section>
     <h2>Welcom</h2>
     <h4>Sign in to your account</h4>
     <form>
       <label>Username<CiUser /></label>
-        <input type="text" className='loginInput'/>
+        <input 
+          onChange={onUserNameChanged} 
+          type="text" 
+          value={username}
+          className='loginInput'
+        />
 
         <label>Password<CiLock /></label>
-        <input type="password" className='loginInput'/>
+        <input
+        onChange={onPasswordChanged} 
+        type="password" 
+        value={password}
+        className='loginInput'
+        />
    
-      <button>Sign in</button>
+      <button onClick={onSubmit} type='button'>Sign in</button>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} 
     </form>
   </section>
   )
--- src/features/users/loginSlice.jsx
+++ src/features/users/loginSlice.jsx
@@ -1,15 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const users = [
+  {
+    username: 'Alex',
+    password: 'randompasswordforalex'
+  },
+  {
+    username: 'Robert',
+    password: 'randompasswordforobert'
+  }
+]
+
 export const isLogged = createSlice({
   name: 'isLogged',
   initialState: {
-    value: false
+
+    value: false,
+    errorMessage: null
   },
   reducers: {
         changeLogged: (state) => {
 state.value = !state.value
-    }
+    },
+authentification: (state, action) => {
+  const { username, password } = action.payload
+const user = users.find((user) => user.username === username && user.password === password)
+  if (user){
+state.value = true
+state.errorMessage=false
+  }else {
+    state.errorMessage = "Invalid username or password";  
+  }
+}
   }
 })
-export const { changeLogged } = isLogged.actions
+export const { changeLogged, authentification } = isLogged.actions
 export default isLogged.reducer
